870a95b408c6aab3d6fbfdfa41554ac2039fe46e
Dennis Orbison <orbison.dennis@gmail.com>
2019-04-02 05:59:41 -0700

    login form connected to app

--- expat-nation/src/App.js
+++ expat-nation/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LoginForm from './Components/Authentication/LoginForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+        <LoginForm />
       </div>
     );
   }
---
+++ expat-nation/src/Components/Authentication/LoginForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LoginForm = props => {
+
+  return <h1>log in here testing</h1>
+}
+
+export default LoginForm;
